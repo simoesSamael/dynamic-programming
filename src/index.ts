dd0bf81c06d454ee799c505ec5ba03e164602281
Samael Simoes <samael__@hotmail.com>
2022-04-19 21:17:33 -0300

    Colocado algumas descrições no codigo

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import { superCalculateFatorial } from './calculate';
 import { calculateSuperFatorial } from './calculateSuperFatorial';
 
 // Calculate the superFatorial with the default implementation, simple code just loops through
+// Mostly get the current time by the now moto
+// Right after I perform the calculation, passing the value 5 and saving the value in the variable
+// Here I get the current time after running all the calculation saved as end time
 const startTime = now();
 const result = superCalculateFatorial(5);
 const end = now();
 
 //Calculate the fatorial dynamic implementation
+
 const vlStartTime = now();
 const vlCalculateFatorial = calculateSuperFatorial();
 const resultCalculateFatorial = vlCalculateFatorial(5);
